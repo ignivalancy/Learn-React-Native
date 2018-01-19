1b2b15a31686943905b875a805d67db667733cbe
Lancy Goyal <lancy.github@ignivasolutions.com>
2018-01-19 16:32:01 +0530

    first session example

--- Demo/App.js
+++ Demo/App.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
 
 const welcomeMessage = 'Welcome to React Native World !';
 
-export default class App extends Component<{}> {
+const WelcomeMessage = ({text}) => {
+  return (
+    <Text style={styles.welcome}>
+      {text}
+    </Text>
+  )
+}
+
+const WelcomeInput = ({onChangeInput}) => {
+  return (
+    <TextInput
+      style={styles.input}
+      onChangeText={onChangeInput}
+      placeholder={welcomeMessage}
+    />
+  )
+}
+
+export default class App extends Component {
+  state = {
+    text: welcomeMessage
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          {welcomeMessage}
-        </Text>
+        <WelcomeMessage {...this.state}/>
+        <WelcomeInput
+          onChangeInput={(text) => this.setState({text})}
+        />
       </View>
     );
   }
@@ -25,10 +48,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    padding: 10,
+    justifyContent: 'space-around'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
+    margin: 10
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
     margin: 10,
+    padding: 10
   }
 });
